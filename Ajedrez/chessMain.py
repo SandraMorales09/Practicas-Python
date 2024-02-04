ffc63d3d8c2d3a47c95828c3d64b63889003db51
SandraMorales09 <114303948+SandraMorales09@users.noreply.github.com>
2024-02-03 22:38:20 -0600

    Add files via upload

---
+++ Ajedrez/chessMain.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+class App(): 
+   def __init__:
+      ventana = tk.Tk()
+      ventana.title("Motor de ajedrez")
+      ventana.iconbitmap("ajedrez.ico")
+
+    
+    def __call__:
+        self.ventana.mainloop()
+
+
+MotorDeAjedrez = App()
+MotorDeAjedrez()
+
+
